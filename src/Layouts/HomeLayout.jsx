c140a47e9087ba2a27a38a6ae5ba2d67958be74e
Aakarshit07 <aakarshit070@gmail.com>
2023-11-04 13:26:37 +0530

    Added logout functionality

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 
 import Footer from "../Components/Footer";
+import { logout } from "../Redux/Slices/AuthSlice";
 
 function HomeLayout({children}) {
 
@@ -31,11 +32,11 @@ function HomeLayout({children}) {
         
     }
 
-    function handleLogout(e) {
+    async function handleLogout(e) {
         e.preventDefault();
 
-        // const res = await dispatch(logout());
-        // if(res?.payload?.success)
+        const res = await dispatch(logout());
+        if(res?.payload?.success)
         navigate("/");
     }
 
--- src/Redux/Slices/AuthSlice.js
+++ src/Redux/Slices/AuthSlice.js
@@ -45,6 +45,20 @@ export const login = createAsyncThunk("/auth/login", async (data) => {
     }
 });
 
+export const logout = createAsyncThunk("/auth/logout", async () => {
+    try {
+        const res = axiosInstance.post("user/logout");
+        toast.promise(res, {
+            loading: "Wait! logout in progress..",
+            success: (data) => {
+                return data?.data?.message;
+            },
+            error: "Failed to logout"
+        });
+    } catch (error) {
+        toast.error(error?.response?.data?.message);
+    }
+})
 
 const authSlice = createSlice({
     name:'auth',
@@ -58,7 +72,13 @@ const authSlice = createSlice({
             state.isLoggedIn = true;
             state.data = action?.payload?.user;
             state.role = action?.payload?.user?.role;
-        });
+        })
+        .addCase(logout.fulfilled, (state) => {
+            localStorage.clear();
+            state.data = {};
+            state.isLoggedIn = false;
+            state.role = "";
+        })
     }
 });
 
