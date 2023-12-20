055049f87e3f50d5555aa6fc8d359eac68887753
Aakarshit07 <aakarshit070@gmail.com>
2023-12-20 16:08:42 +0530

    Added course lectures

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Contact from './Pages/Contact';
 import CourseDescription from './Pages/Course/CourseDescription';
 import CourseList from './Pages/Course/CourseList';
 import CreateCourse from './Pages/Course/CreateCourse';
+import Displaylectures from './Pages/Dashboard/Displaylectures';
 import Denied from './Pages/Denied';
 import HomePage from './Pages/HomePage';
 import Login from './Pages/Login';
@@ -46,6 +47,7 @@ function App() {
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/checkout/success" element={<CheckoutSuccess />} />
           <Route path="/checkout/fail" element={<CheckoutFailure />} />
+          <Route path="/course/displaylectures" element={<Displaylectures />} />
 
 
         
